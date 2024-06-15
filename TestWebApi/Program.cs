1807918f11014f041e2a9321c98315da83dc8540
OmarAl-Khateeb <oalkhateeb2001@gmail.com>
2024-06-15 15:34:41 +0300

    splitting webapi and testworker

---
+++ TestWebApi/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using TestWorkerService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<SensorDataContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+app.MapControllers();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.Run();
--- TestWorkerService/Program.cs
+++ TestWorkerService/Program.cs
@@ -1,18 +1,15 @@
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using TestWorkerService;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
-builder.Services.AddControllers();
+
 builder.Services.AddDbContext<SensorDataContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddSwaggerGen();
-var app = builder.Build();
-app.MapControllers();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-app.Run();
+
+
+var host = builder.Build();
+host.Run();
