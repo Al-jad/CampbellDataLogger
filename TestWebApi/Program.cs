d6e1dbd4ff03013e8fba03a52f1e479192bb52c9
OmarAl-Khateeb <oalkhateeb2001@gmail.com>
2024-06-25 13:46:00 +0300

    lets do this properly

--- TestWebApi/Program.cs
+++ TestWebApi/Program.cs
@@ -5,6 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAnyOrigin",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
 builder.Services.AddDbContext<SensorDataContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -12,7 +19,7 @@ builder.Services.AddDbContext<SensorDataContext>(options =>
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
 app.MapControllers();
-app.UseCors(opt => opt.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.UseCors("AllowAnyOrigin");
 app.UseSwagger();
 app.UseSwaggerUI();
 app.Run();
